676f4d9629c84f4b9bce5575623b2011d96dfb8a
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2023-01-25 16:37:14 +0100

    Remove unnecessary imports from prisma.ts

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from "@prisma/client"
-import { logger } from "../utils/logger.js"
-import { blue } from "colorette"
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
